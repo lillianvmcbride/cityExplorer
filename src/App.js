16bc987960e9ad7145d0f7392e46e555a119b413
Lilly McBride <lillianvioletmcbride@gmail.com>
2022-05-17 14:43:47 -0500

    starting netlify

--- src/App.js
+++ src/App.js
@@ -6,10 +6,37 @@
 import React from "react";
 import axios from "axios";
 
-class App extends React.Componentt {
+class App extends React.Component {
 
-  constructor(props)
+  constructor(props) {
   super(props);
+  this.state = {
+    searchQuery: "",
+    location: {place_id: "unknown", display_name: "None"},
+  };
+}
+
+getLocation = async () => {
+  const API = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_CITY_KEY}&q=${this.state.searchQuery}&format=json`;
+  const res = await axios.get(API);
+  this.setState({ location: res.data[0] });
+};
+
+render() {
+  console.log(`Location: ${JSON.stringify(this.state.location)}`);
+  return (
+    <>
+      <input
+        onChange={(e) => this.setState({ searchQuery: e.target.value })}
+        placeholder="search for a city"
+      />
+      <button onClick={this.getLocation}>Explore!</button>
+      {this.state.location.place_id && (
+        <h2>The city is: {this.state.location.display_name}</h2>
+      )}
+    </>
+  );
+}
 }
 
 export default App;
