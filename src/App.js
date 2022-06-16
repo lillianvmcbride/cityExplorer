69b68800c57f4a03ae2701707294dffb3c842967
Lilly McBride <lillianvioletmcbride@gmail.com>
2022-06-16 01:01:08 -0500

    lab 6 functionality

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@
 
 import "./App.css";
 import React from "react";
-// import axios from "axios";
-// import Header from"./Header";
+import axios from "axios";
+import Header from"./Header";
 import Input from "./Input";
-// import Map from "./Map";
+import Map from "./Map";
 import Errors from "./Errors";
 import DataDisplay from "./DataDisplay";
-// // import {Container, Col, Row} from "react-bootstrap";
+// import {Container, Col, Row} from "react-bootstrap";
 
 class App extends React.Component {
 
@@ -27,9 +27,9 @@ class App extends React.Component {
 }
 
 
-// handleChange = (location) => {
-//   this.setState({searchQuery: location}, this.getLocation);
-// }
+handleChange = (location) => {
+  this.setState({searchQuery: location}, this.getLocation);
+}
 
 
 // componentDidMount() {
@@ -48,15 +48,15 @@ class App extends React.Component {
 * @param {string} city - the name of a city
 * @returns {object} - the JSON object with the data
 */
-callBackEndAPI = async (location) => {
- const response = await fetch("http://localhost:5000/weather?searchQuery=" + location);
- const body = await response.json();
- // Make sure things worked!
- if (response.status !== 200) {
-   throw Errors(body.message);
- }
- return body;
-};
+// callBackEndAPI = async (location) => {
+//  const response = await fetch("http://localhost:5000/weather?searchQuery=" + location);
+//  const body = await response.json();
+//  // Make sure things worked!
+//  if (response.status !== 200) {
+//    throw Errors(body.message);
+//  }
+//  return body;
+// };
 
 /*
 * @param {string} city - the clity to look for
@@ -64,57 +64,57 @@ callBackEndAPI = async (location) => {
 handleSearch = async (location) => {
  console.log(`App.handleSearch() location: ${location}`);
  this.setState({ location: location });
- this.callBackEndAPI(location)
-   .then((res) => {
-     console.log(res.express);
-     this.setState({ data: res.express });
-   })
-   .catch((err) => {
-     console.log(err);
-   });
+//  this.callBackEndAPI(location)
+  //  .then((res) => {
+  //    console.log(res.express);
+  //    this.setState({ data: res.express });
+  //  })
+  //  .catch((err) => {
+  //    console.log(err);
+  //  });
  console.log(`App.handleSearch() results: ${this.state.data}`);
 };
 
+getLocation = async () => {
+  let searchQuery = this.state.searchQuery;
+  const API = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_CITY_KEY}&q=${searchQuery}&format=json`;
+  console.log(`App.getLocation() API = ${API}`);
+  await axios.get(API)
+    .then ( (res) => this.setState({ location: res.data[0], error : null }))
+    .catch( (res) => {
+      this.state.error = res;
+      this.setState ({error : res});
+      console.log(`getLocation error`);
+  });
+};
+
 render() {
-  console.log("app render " + JSON.stringify(this.state.data));
  return (
    <div className="App">
-     <header>
-       <h1 className="h1">City Explorer</h1>
-     </header>
-     <Input handleSearch={this.handleSearch} />
+     <Header header="City Explorer"/>
+     <Input handleChange = {this.handleChange} handleSearch={this.handleSearch} />
+     {this.state.location.place_id && 
+        <><h2>Location is: {this.state.location.display_name}</h2><h2>Latitude is: {this.state.location.lat}</h2><h2>Longitude is: {this.state.location.lon}</h2>
+        <Map img_url={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_CITY_KEY}&center=${this.state.location.lat},${this.state.location.lon}&zoom=12`} 
+        city={this.state.location.display_name}
+        />
+        </>
+      }
+         {/* {this.state.location.place_id} && 
+         <h2 className="h2">Location: {this.state.location.display_name}</h2>
+         <h2 className="h2">Latitude: {this.state.location.lat}</h2>
+         <h2 className="h2">Longitude: {this.state.location.lon}</h2>
+         <Map img_url={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_CITY_KEY}&center=${this.state.location.lat},${this.state.location.lon}&zoom=12`} 
+        city={this.state.location.display_name}
+        /> */}
      {this.state.data !== null && <DataDisplay data={this.state.data} />}
+     {this.state.error && 
+      <Errors errorMessage = {this.state.error}/>}
    </div>
  );
 }
 
 
-
-// getLocation = async () => {
-//   let searchQuery = this.state.searchQuery;
-  // const API = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_CITY_KEY}&q=${searchQuery}&format=json`;
-  // console.log(`App.getLocation() API = ${API}`);
-  // await axios.get(API)
-//     .then ( (res) => this.setState({ location: res.data[0], error : null }))
-//     .catch( (res) => {
-//       this.state.error = res;
-//       this.setState ({error : res});
-//       console.log(`getLocation error`);
-//   });
-// };
-
-// render() {
-//   return (
-//     <div className="App">
-//        <Header header="City Explorer"/>
-//        <Input handleChange = {this.handleChange}/>
-//       {this.state.location.place_id && 
-//         <><h2 className="h2">Location: {this.state.location.display_name}</h2>
-//         <h2 className="h2">Latitude: {this.state.location.lat}</h2>
-//         <h2 className="h2">Longitude: {this.state.location.lon}</h2>
-//         <Map img_url={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_CITY_KEY}&center=${this.state.location.lat},${this.state.location.lon}&zoom=12`} 
-//         city={this.state.location.display_name}
-//         />
 //         </>
 //       }
 //       {this.state.error && 
@@ -122,6 +122,7 @@ render() {
 //     </div>
 //   );
 // }
+
  }
 
 export default App;
