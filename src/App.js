7897540617cac183a35cc6299527b76eeac1bb60
Lilly McBride <lillianvioletmcbride@gmail.com>
2022-06-16 10:31:21 -0500

    lab 7 recovered

--- src/App.js
+++ src/App.js
@@ -32,31 +32,23 @@ handleChange = (location) => {
 }
 
 
-// componentDidMount() {
-//   if (this.state.location !== null) {
-//     this.callBackendAPI(this.state.location)
-//       .then((res) => {
-//         this.setState({ data: res.express });
-//       })
-//       .catch((err) => {
-//         console.log(err);
-//       });
-//   }
-// }
 
 /*
 * @param {string} city - the name of a city
 * @returns {object} - the JSON object with the data
 */
-// callBackEndAPI = async (location) => {
-//  const response = await fetch("http://localhost:5000/weather?searchQuery=" + location);
-//  const body = await response.json();
-//  // Make sure things worked!
-//  if (response.status !== 200) {
-//    throw Errors(body.message);
-//  }
-//  return body;
-// };
+
+
+
+callBackEndAPI = async (location) => {
+ const response = await fetch("http://localhost:5000/weather?searchQuery=" + location);
+ const body = await response.json();
+ // Make sure things worked!
+ if (response.status !== 200) {
+   throw Errors(body.message);
+ }
+ return body;
+};
 
 /*
 * @param {string} city - the clity to look for
@@ -64,14 +56,14 @@ handleChange = (location) => {
 handleSearch = async (location) => {
  console.log(`App.handleSearch() location: ${location}`);
  this.setState({ location: location });
-//  this.callBackEndAPI(location)
-  //  .then((res) => {
-  //    console.log(res.express);
-  //    this.setState({ data: res.express });
-  //  })
-  //  .catch((err) => {
-  //    console.log(err);
-  //  });
+ this.callBackEndAPI(location)
+   .then((res) => {
+     console.log(res.express);
+     this.setState({ data: res.express });
+   })
+   .catch((err) => {
+     console.log(err);
+   });
  console.log(`App.handleSearch() results: ${this.state.data}`);
 };
 
@@ -92,36 +84,21 @@ render() {
  return (
    <div className="App">
      <Header header="City Explorer"/>
-     <Input handleChange = {this.handleChange} handleSearch={this.handleSearch} />
+     <Input className handleChange = {this.handleChange} handleSearch={this.handleSearch} />
      {this.state.location.place_id && 
-        <><h2>Location is: {this.state.location.display_name}</h2><h2>Latitude is: {this.state.location.lat}</h2><h2>Longitude is: {this.state.location.lon}</h2>
+        <><h2>Location: {this.state.location.display_name}</h2><h2>Latitude: {this.state.location.lat}</h2><h2>Longitude: {this.state.location.lon}</h2>
         <Map img_url={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_CITY_KEY}&center=${this.state.location.lat},${this.state.location.lon}&zoom=12`} 
         city={this.state.location.display_name}
         />
         </>
       }
-         {/* {this.state.location.place_id} && 
-         <h2 className="h2">Location: {this.state.location.display_name}</h2>
-         <h2 className="h2">Latitude: {this.state.location.lat}</h2>
-         <h2 className="h2">Longitude: {this.state.location.lon}</h2>
-         <Map img_url={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_CITY_KEY}&center=${this.state.location.lat},${this.state.location.lon}&zoom=12`} 
-        city={this.state.location.display_name}
-        /> */}
+
      {this.state.data !== null && <DataDisplay data={this.state.data} />}
      {this.state.error && 
       <Errors errorMessage = {this.state.error}/>}
    </div>
  );
 }
-
-
-//         </>
-//       }
-//       {this.state.error && 
-//       <Errors errorMessage = {this.state.error}/>}
-//     </div>
-//   );
-// }
 
  }
 
